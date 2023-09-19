c2ff8c4725b4849e645f021fe8769396c0a6787a
rob-writes-code <robert_phillips17@hotmail.co.uk>
2023-09-19 15:13:25 +0100

    basic routing

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('/templates/index.html')
+    return render_template('index.html')
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("index"))
+
+if __name__ == "__main__":
+    app.run()
