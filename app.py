66e03f5b233c2e4cba533b00be59f654478e40a5
rob-writes-code <robert_phillips17@hotmail.co.uk>
2023-09-20 13:42:46 +0100

    dynamic html with jinja

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/<name>')
+def index(name):
+    return render_template('index.html', content=name)
 
 @app.route("/admin")
 def admin():
