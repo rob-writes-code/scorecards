208c40695f3ac8fe8fa3558778f4bfedc3662b4c
rob-writes-code <robert_phillips17@hotmail.co.uk>
2023-09-22 11:42:13 +0100

    Flash messages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request, session
+from flask import Flask, render_template, redirect, url_for, request, session, flash
 from datetime import timedelta
 
 app = Flask(__name__)
@@ -20,14 +20,17 @@ def login():
         # to access info from a form: dict key relates to input name on html form
         user = request.form["nm"]
         session["user"] = user
+        flash(f"Successfully logged in, {user}!", "info")
         return redirect(url_for("user"))
     else:
         if "user" in session:
+            flash(f"Already logged in!", "info")
             return redirect(url_for("user"))
         return render_template("login.html")
     
 @app.route("/logout")
 def logout():
+    flash("Successfully logged out!", "info")
     session.pop("user", None)
     return redirect(url_for("login"))
 
@@ -35,8 +38,9 @@ def logout():
 def user():
     if "user" in session:
         user = session["user"]
-        return f"<h1>{user}</h1>"
+        return render_template("user.html", user=user)
     else:
+        flash("You are not logged in!", "info")
         return redirect(url_for("login"))
 
 if __name__ == "__main__":
