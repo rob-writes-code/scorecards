696952cfd3f189743d87f4378d6ed02b592762e6
rob-writes-code <robert_phillips17@hotmail.co.uk>
2023-09-22 10:35:38 +0100

    forms and post requests

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-@app.route('/<name>')
-def index(name):
-    return render_template('index.html', content=name)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route("/admin")
 def admin():
     return redirect(url_for("index"))
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == "POST":
+        # to access info from a form: dict key relates to input name on html form
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route('/<usr>')
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
