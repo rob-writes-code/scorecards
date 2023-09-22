14be441b186971f7e250550c726b24a110c6c764
rob-writes-code <robert_phillips17@hotmail.co.uk>
2023-09-22 11:06:40 +0100

    flask sessions for storing user

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 
 app = Flask(__name__)
+app.secret_key = "hello"
 
 @app.route('/')
 def index():
@@ -15,13 +16,25 @@ def login():
     if request.method == "POST":
         # to access info from a form: dict key relates to input name on html form
         user = request.form["nm"]
-        return redirect(url_for("user", usr=user))
+        session["user"] = user
+        return redirect(url_for("user"))
     else:
+        if "user" in session:
+            return redirect(url_for("user"))
         return render_template("login.html")
+    
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
-@app.route('/<usr>')
-def user(usr):
-    return f"<h1>{usr}</h1>"
+@app.route('/user')
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    else:
+        return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(debug=True)
